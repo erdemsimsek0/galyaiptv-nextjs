671b3c0664191abc2674c5963fe185275c67ea77
erdemsimsek0 <erdemsimsek06@icloud.com>
2026-03-10 02:27:07 +0300

    Update scraper.ts

--- lib/scraper.ts
+++ lib/scraper.ts
@@ -1,1 +1,7 @@
-
+export async function getIPTVInfo() {
+  return {
+    url: 'http://panel.galyaiptv.com.tr:8080',
+    username: `galya_${Math.random().toString(36).slice(2, 8)}`,
+    password: Math.random().toString(36).slice(2, 10).toUpperCase(),
+  };
+}
