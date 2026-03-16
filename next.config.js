18a78ea6b50a097d2dd5bd622e88c4542a1ecdd2
erdemsimsek0 <erdemsimsek06@icloud.com>
2026-03-16 06:02:09 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+// HTTP sunuculara fetch için gerekli
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const nextConfig = {
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: 'http', hostname: 'pro4kiptv.xyz' },
-      { protocol: 'https', hostname: 'pro4kiptv.xyz' },
+      { protocol: 'http',  hostname: 'pro4kiptv.xyz', port: '2086' },
+      { protocol: 'https', hostname: 'pro4kiptv.xyz', port: '2086' },
+      { protocol: 'http',  hostname: '**' },
     ],
   },
   compress: true,
   poweredByHeader: false,
-  // HTTP sunuculara erişim için
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
 };
 
 module.exports = nextConfig;
