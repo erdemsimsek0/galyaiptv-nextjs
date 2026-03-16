88c9eac9160a15ac9e4da317759485ae0f0c11d2
erdemsimsek0 <erdemsimsek06@icloud.com>
2026-03-16 05:55:18 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,17 @@
 const nextConfig = {
   images: {
     unoptimized: true,
+    remotePatterns: [
+      { protocol: 'http', hostname: 'pro4kiptv.xyz' },
+      { protocol: 'https', hostname: 'pro4kiptv.xyz' },
+    ],
   },
   compress: true,
   poweredByHeader: false,
+  // HTTP sunuculara erişim için
+  experimental: {
+    serverComponentsExternalPackages: [],
+  },
 };
 
 module.exports = nextConfig;
